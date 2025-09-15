01fbbfd01c253b3afed9e1688d685d40fc7a15d2
David Y. Stephenson <david@davidystephenson.com>
2025-09-14 20:07:37 -0400

    Debug activity

--- src/useActive.ts
+++ src/useActive.ts
@@ -5,5 +5,8 @@ export default function useActive (props: ClinkRobeProps): boolean {
   const location = useLocation()
   const routeActive = location.pathname === props.to
   const active = props.active ?? routeActive
+  if (props.debug) {
+    console.debug('[clink-robes] useActive', { props, location, routeActive, active })
+  }
   return active
 }
